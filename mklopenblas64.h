beeaded8d7371d0054ec8fe4023686172073af1b
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-03-11 12:57:25 -0500

    add mklopenblas64.h

---
+++ mklopenblas64.h
@@ -0,0 +1,13 @@
+#pragma once
+#define MKL_ILP64
+// #include "mkl.h"
+
+#include "/usr/include/mkl/mkl_blas.h"
+#include "/usr/include/mkl/mkl_cblas.h"
+#include "/usr/include/mkl/mkl_lapack.h"
+#include "/usr/include/mkl/mkl_lapacke.h"
+#include "/usr/include/mkl/mkl_trans.h"
+#include "/usr/include/mkl/mkl_service.h"
+
+// #include "/usr/include/lapacke_utils.h"
+// #include "/usr/include/lapacke.h"
